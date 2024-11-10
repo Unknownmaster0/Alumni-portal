bfb07409f24ed206a30adc43f5081759fee14ee1
Unknownmaster0 <sagar192002singh@gmail.com>
2024-11-10 20:41:33 +0530

    starter of the bacend part of the alumni portal

---
+++ backend/src/db/index.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+async function connectDb() {
+  try {
+    const connectionObj = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${process.env.DB_NAME}`
+    );
+    // console.log(`connection obj of database`);
+    // console.log(connectionObj);
+    // console.log(`database is connected: ${connectionObj.connection.host}`);
+  } catch (err) {
+    console.error("error while connection with database: ", err);
+    throw err;
+  }
+}
+
+module.exports = {
+  connectDb,
+};
---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const { connectDb } = require("./db/index");
+
+const port = process.env.PORT;
+
+connectDb()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`server is listening on port: ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error(`error while connection with database: ${err}`);
+  });
